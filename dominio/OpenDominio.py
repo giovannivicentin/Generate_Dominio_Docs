c5a4d28f6ea2a76489bd3f79749a4c2ced100d81
Giovanni Fernandes Vicentin <giovannifvicentin@gmail.com>
2023-04-06 15:37:10 -0300

    Update OpenDominio.py

--- dominio/OpenDominio.py
+++ dominio/OpenDominio.py
@@ -1,32 +1,29 @@
 import subprocess
 
 class OpenDominio:
-    def __init__(self):
-        self.module = None
-        self.path = None
+    MODULES = {
+        'folha': 'Folha.rdp',
+        'fiscal': 'Fiscal.rdp',
+        'contabil': 'Contabil.rdp'
+    }
 
-    def open_dominio(self):
-        self.path = f'C:\\Dominio\\{self.module}'
+    def __init__(self):
+        pass
+
+    def open_module(self, module):
+        if module not in self.MODULES:
+            raise ValueError("Invalid module name")
+        path = f"./Dominio/{self.MODULES[module]}"
         try:
-            subprocess.run(["mstsc", self.path])
+            subprocess.run(["mstsc", path])
         except FileNotFoundError:
             print("Rdp file not found. Verify the path.")
         except Exception as e:
             print("Error occurred while opening the .rdp file:", e)
 
-    def open_folha(self):
-        self.module = 'Folha.rdp'
-        return self.open_dominio()
 
-    def open_fiscal(self):
-        self.module = 'Fiscal.rdp'
-        return self.open_dominio()
+opener = OpenDominio()
 
-    def open_contabil(self):
-        self.module = 'Contabil.rdp'
-        return self.open_dominio()
-
-    def open_contabil(self):
-        self.module = 'Contabil.rdp'
-        return self.open_dominio()
-
+OpenFolha = opener.open_module('folha')
+OpenFiscal = opener.open_module('fiscal')
+OpenContabil = opener.open_module('contabil')
